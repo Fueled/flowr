4e164a943c3cafbffe4ec37dcb8d5488dbf28ffb
julien|fueled <julien@fueled.com>
2017-04-21 12:16:08 -0400

    feat(MainAcitivty):  add setCustomTransition sample

--- sample/src/main/java/com/fueled/flowr/sample/HomeFragment.java
+++ sample/src/main/java/com/fueled/flowr/sample/HomeFragment.java
@@ -4,7 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.view.View;
 
 import com.fueled.flowr.NavigationIconType;
-import com.fueled.flowr.sample.R;
 import com.fueled.flowr.sample.core.AbstractFragment;
 import com.fueled.flowr.sample.databinding.FragmentHomeBinding;
 
@@ -14,36 +13,37 @@ import com.fueled.flowr.sample.databinding.FragmentHomeBinding;
  */
 public class HomeFragment extends AbstractFragment implements View.OnClickListener {
 
-	private FragmentHomeBinding binding;
+    private FragmentHomeBinding binding;
 
-	@Override
-	public int getLayoutId() {
-		return R.layout.fragment_home;
-	}
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_home;
+    }
 
-	@Override
-	protected void setupView(View view) {
-		binding = DataBindingUtil.bind(view);
-		binding.homeOpenViewButton.setOnClickListener(this);
-	}
+    @Override
+    protected void setupView(View view) {
+        binding = DataBindingUtil.bind(view);
+        binding.homeOpenViewButton.setOnClickListener(this);
+    }
 
-	@Override
-	public boolean onNavigationIconClick() {
-		return true;
-	}
+    @Override
+    public boolean onNavigationIconClick() {
+        return true;
+    }
 
-	@Override
-	public NavigationIconType getNavigationIconType() {
-		return NavigationIconType.HIDDEN;
-	}
+    @Override
+    public NavigationIconType getNavigationIconType() {
+        return NavigationIconType.HIDDEN;
+    }
 
-	@Override
-	public void onClick(View view) {
-		displayViewFragment();
-	}
+    @Override
+    public void onClick(View view) {
+        displayViewFragment();
+    }
 
-	private void displayViewFragment() {
-		getRouter().open(ViewFragment.class)
-				.displayFragment();
-	}
+    private void displayViewFragment() {
+        getRouter().open(ViewFragment.class)
+                .setCustomTransactionAnimation(android.R.anim.fade_in, android.R.anim.fade_out, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .displayFragment();
+    }
 }
